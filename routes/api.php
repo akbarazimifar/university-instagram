712dcd5f4d891351c7c12a3b1a29beaaf0d84db0
Hooman Hasanzadeh <mr.subscript@gmail.com>
2018-10-24 16:36:29 +0330

    Updated api.php

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,7 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware(['auth:api', 'username'])->group(function () {
+    Route::get('{username}', 'UserController@show')->name('user.profile');
+    Route::get('{username}/medias', 'MediaController@getUserMedias')->name('user.medias');
 });
