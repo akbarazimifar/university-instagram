ad118438a030acafad4bc70bd49f44cb9d225354
Hooman Hasanzadeh <mr.subscript@gmail.com>
2018-10-24 09:26:19 +0330

    Created medias table migration

---
+++ database/migrations/2018_10_24_031832_create_medias_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMediasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('medias', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('user_id');
+            $table->integer('width');
+            $table->integer('height');
+            $table->string('file_path');
+            $table->string('thumb_path');
+            $table->longText('caption')->nullable();
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('medias');
+    }
+}
