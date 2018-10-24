c7448d1a524449d7f27c244f329d882f1a5b00e1
Hooman Hasanzadeh <mr.subscript@gmail.com>
2018-10-24 09:26:47 +0330

    Created users profiles table migration

---
+++ database/migrations/2018_10_24_040417_create_users_profiles_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersProfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users_profiles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('user_id')->unique();
+            $table->string('thumb_path');
+            $table->string('file_path');
+            $table->integer('width');
+            $table->integer('height');
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users_profiles');
+    }
+}
