8ba668571783d6e60de5c2b470539dbba51455fe
Hooman Hasanzadeh <mr.subscript@gmail.com>
2018-10-24 12:22:20 +0330

    Added username field to users

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -5,11 +5,11 @@ use Illuminate\Support\Facades\Hash;
 
 $factory->define(App\User::class, function (Faker $faker) {
     return [
-        'first_name' => $faker->firstName,
-        'last_name' => $faker->lastName,
-        'email' => $faker->unique()->safeEmail,
-        // 'email_verified_at' => now(),
-        'password' => Hash::make('123456'),
+        'username'       => $faker->unique()->userName,
+        'first_name'     => $faker->firstName,
+        'last_name'      => $faker->lastName,
+        'email'          => $faker->unique()->safeEmail,
+        'password'       => Hash::make('123456'),
         'remember_token' => str_random(10),
     ];
 });
