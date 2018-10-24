6b3eaf32f69a3b239c6b4bacbf39e046920c730a
Hooman Hasanzadeh <mr.subscript@gmail.com>
2018-10-24 16:55:40 +0330

    Added withCount

--- app/Http/Controllers/MediaController.php
+++ app/Http/Controllers/MediaController.php
@@ -8,10 +8,9 @@ class MediaController extends Controller
 {
     public function getUserMedias(Request $request)
     {
-        try{
-            return $request->get('user_object')->medias()->paginate(config('instagram.paginate_per_page.medias'));
-        }catch (\Exception $e){
-            dd($e->getMessage());
-        }
+        return $request->get('user_object')
+            ->with('medias')
+            ->withCount('medias')
+            ->paginate(config('instagram.paginate_per_page.medias'));
     }
 }
