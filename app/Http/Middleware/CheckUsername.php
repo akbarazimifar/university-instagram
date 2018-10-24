a18a2f5f18bb0e52c40a86e95c8f9f177cf90746
Hooman Hasanzadeh <mr.subscript@gmail.com>
2018-10-24 16:37:56 +0330

    Updated CheckUsername Middleware

--- app/Http/Middleware/CheckUsername.php
+++ app/Http/Middleware/CheckUsername.php
@@ -10,15 +10,22 @@ class CheckUsername
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
      * @return mixed
      */
     public function handle($request, Closure $next)
     {
-        if ($request->route('username')){
+        if ($request->route('username')) {
             $username = $request->route('username');
-            User::where('username', '=', $username)->firstOrFail();
+            $user_eloquent = User::where('username', '=', $username);
+            $request->attributes->add([
+                'user_eloquent' => $user_eloquent
+            ]);
+            $user_object = $user_eloquent->firstOrFail();
+            $request->attributes->add([
+                'user_object' => $user_object
+            ]);
         }
         return $next($request);
     }
