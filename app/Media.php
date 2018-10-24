dcc85767dcb350032fe885d991d03cbe9f40ab6c
Hooman Hasanzadeh <mr.subscript@gmail.com>
2018-10-24 09:27:28 +0330

    Created Media model

---
+++ app/Media.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Media extends Model
+{
+    protected $table = 'medias';
+
+    protected $fillable = [
+        'user_id', 'width', 'height', 'file_path', 'thumb_path', 'caption'
+    ];
+
+    protected $hidden = [
+        'user_id',
+        'updated_at'
+    ];
+
+    public function likes()
+    {
+        return $this->belongsToMany(User::class, 'medias_likes', 'media_id', 'user_id')->with('profile');
+    }
+}
