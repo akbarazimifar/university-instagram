71d47b4457de9192648b3eee4f565bda0b5d9586
Hooman Hasanzadeh <mr.subscript@gmail.com>
2018-10-24 16:57:14 +0330

    Created UserTest

---
+++ tests/Unit/UserTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\User;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Laravel\Passport\Passport;
+
+class UserTest extends TestCase
+{
+    private static $user;
+
+    public function testUserProfile()
+    {
+        self::$user = User::first();
+        Passport::actingAs(self::$user);
+        $response = $this->call('get', Route('user.profile', self::$user->username));
+        $response->assertStatus(200);
+        $response->assertJsonStructure([
+            'id',
+            'username',
+            'first_name',
+            'last_name',
+            'profile_status',
+            'profile' => [
+                'file_path',
+                'thumb_path',
+                'width',
+                'height'
+            ]
+        ]);
+    }
+}
