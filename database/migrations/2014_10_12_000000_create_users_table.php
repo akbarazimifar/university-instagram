322360e3fbe5a81420e3933001366b4199773ad7
Hooman Hasanzadeh <mr.subscript@gmail.com>
2018-10-24 09:25:10 +0330

    Updated create_users_table migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,12 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name')->nullable();
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            // $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('profile_status', ['PUBLIC', 'PRIVATE'])->default('PUBLIC');
             $table->rememberToken();
             $table->timestamps();
         });
