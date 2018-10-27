a0a934cc032cb58a823b8d6bcf1bd243c4af7b7a
Hooman Hasanzadeh <mr.subscript@gmail.com>
2018-10-27 08:18:06 +0330

    Completed follow method

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\User;
+use App\UserRelationship;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -11,14 +11,50 @@ class UserController extends Controller
     public function follow(Request $request)
     {
         $user = $request->get('user_object');
-        if ($user->id === Auth::user()->id) return response('You can\'t follow your self.',400);
-
-        // TODO check user is followed or not
-
-        // TODO check if there is an ongoing follow request that need to be accepted or not
-
-        // TODO insert follow data to user relationship table
-
+        if ($user->id === Auth::user()->id) return response('You can\'t follow your self.', 400);
+        // check user is followed or not
+        try {
+            $relationship = UserRelationship::where('follower_id', '=', Auth::user()->id)
+                ->where('following_id', '=', $user->id)
+                ->firstOrFail();
+            if ($relationship->is_accepted) {
+                return response([
+                    'ok'          => true,
+                    'status_code' => 200,
+                    'description' => 'User is already followed.'
+                ], 200);
+            } else {
+                return response([
+                    'ok'          => true,
+                    'status_code' => 202,
+                    'description' => 'There is an ongoing follow request that need to be accepted.'
+                ], 202);
+            }
+        } catch (\Exception $e) {
+            if ($user->profile_status === "PUBLIC") {
+                $is_accepted = true;
+            } else {
+                $is_accepted = false;
+            }
+            try {
+                UserRelationship::create([
+                    'follower_id'  => Auth::user()->id,
+                    'following_id' => $user->id,
+                    'is_accepted'  => $is_accepted
+                ]);
+                return response([
+                    'ok'          => true,
+                    'status_code' => 201,
+                    'description' => 'The follow request has been sent.'
+                ], 201);
+            } catch (\Exception $e) {
+                return response([
+                    'ok'          => false,
+                    'status_code' => 500,
+                    'description' => 'There is a problem with the follow request'
+                ], 500);
+            }
+        }
     }
 
 
--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Unit;
 
 use App\User;
+use App\UserRelationship;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -33,6 +34,56 @@ class UserTest extends TestCase
                 'width',
                 'height'
             ]
+        ]);
+    }
+
+    public function testFollow()
+    {
+        // delete all relationships
+        UserRelationship::truncate();
+
+        Passport::actingAs(self::$user);
+
+        // change profile status to private
+        $request_to_follow_user = User::findOrFail(2);
+        $request_to_follow_user->profile_status = "PRIVATE";
+        $request_to_follow_user->save();
+
+        // send the request
+        $response = $this->call('patch', Route('user.follow', $request_to_follow_user->username));
+        $response->assertStatus(201);
+        $response->assertJsonStructure(['ok', 'status_code', 'description']);
+        $response->assertJson([
+            'ok' => true,
+            'status_code' => 201,
+            'description' => 'The follow request has been sent.'
+        ]);
+
+        // resend the request
+        $response = $this->call('patch', Route('user.follow', $request_to_follow_user->username));
+        $response->assertStatus(202);
+        $response->assertJsonStructure(['ok', 'status_code', 'description']);
+        $response->assertJson([
+            'ok' => true,
+            'status_code' => 202,
+            'description' => 'There is an ongoing follow request that need to be accepted.'
+        ]);
+
+        // accept the follow request
+        $relationship = UserRelationship::where('follower_id','=',self::$user->id)
+            ->where('following_id','=',$request_to_follow_user->id)
+            ->firstOrFail();
+        $relationship->is_accepted = true;
+        $relationship->save();
+
+        // resend the request
+        $response = $this->call('patch', Route('user.follow', $request_to_follow_user->username));
+        $response->assertStatus(200);
+        $response->assertJsonStructure(['ok', 'status_code', 'description']);
+        $response->assertJson([
+            'ok' => true,
+            'status_code' => 200,
+            'description' => 'User is already followed.'
         ]);
     }
 }
