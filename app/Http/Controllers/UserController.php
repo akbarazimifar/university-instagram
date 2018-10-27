f89a09bfb1182d0cf0cd0a44dec6239fab85427d
Hooman Hasanzadeh <mr.subscript@gmail.com>
2018-10-27 06:31:11 +0330

    Added some todo comments to follow method

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,11 +4,20 @@ namespace App\Http\Controllers;
 
 use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
     public function follow(Request $request)
     {
+        $user = $request->get('user_object');
+        if ($user->id === Auth::user()->id) return response('You can\'t follow your self.',400);
+
+        // TODO check user is followed or not
+
+        // TODO check if there is an ongoing follow request that need to be accepted or not
+
+        // TODO insert follow data to user relationship table
 
     }
 
