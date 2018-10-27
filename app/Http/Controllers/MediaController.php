7cc4de0d9423669f615c0e1f374a0d033d17a433
Hooman Hasanzadeh <mr.subscript@gmail.com>
2018-10-27 14:45:28 +0330

    Completed getUserMediaLikes method

--- app/Http/Controllers/MediaController.php
+++ app/Http/Controllers/MediaController.php
@@ -3,10 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Media;
+use App\MediaLike;
 use App\User;
-use App\UserRelationship;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 
 class MediaController extends Controller
 {
@@ -25,13 +24,38 @@ class MediaController extends Controller
         $result = User::checkUserViewPermission($user);
         if (!empty($result)) return $result;
         try {
-            return Media::whereId((int) $request->route('id'))
+            return Media::whereId((int)$request->route('id'))
                 ->whereUserId($user->id)
                 ->withCount('likes')
                 ->with(['likes'])
                 ->firstOrFail();
         } catch (\Exception $e) {
             return response(['ok' => false, 'description' => 'Unknown media id.'], 400);
+        }
+    }
+
+    public function getUserMediaLikes(Request $request)
+    {
+        $user = $request->get('user_object');
+        $result = User::checkUserViewPermission($user);
+        if (!empty($result)) return $result;
+        try {
+            $result = MediaLike::whereMediaId((int)$request->route('id'))
+                ->whereUserId($user->id)
+                ->with(['user', 'user.profile'])
+                ->get();
+            if (empty($result->toArray())) return response([
+                'ok' => true,
+                'status_code' => 200,
+                'description' => 'This media does not have any likes'
+            ], 200);
+            return $result;
+        } catch (\Exception $e) {
+            return response([
+                'ok'          => false,
+                'status_code' => 400,
+                'description' => 'Wrong media or user id.'
+            ], 400);
         }
     }
 }
--- tests/Unit/MediaTest.php
+++ tests/Unit/MediaTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Unit;
 
 use App\Media;
+use App\MediaLike;
 use App\User;
 use Laravel\Passport\Passport;
 use Tests\TestCase;
@@ -75,5 +76,57 @@ class MediaTest extends TestCase
             default:
                 $this->fail('Unknown header status code returned (' . $response->getStatusCode() . ').');
         }
+    }
+
+    public function testUserMediaLikes()
+    {
+        // delete all likes
+        MediaLike::truncate();
+        Passport::actingAs(self::$user);
+
+        $media = Media::where('user_id', '=', self::$user->id)->firstOrFail();
+        $parameters = [
+            'username' => self::$user->username,
+            'id'       => $media->id
+        ];
+        $response = $this->call('get', Route('user.media.likes', $parameters));
+        $response->assertStatus(200);
+        $response->assertJsonStructure(['ok', 'status_code', 'description']);
+        $response->assertJson([
+            'ok'          => true,
+            'status_code' => 200,
+            'description' => 'This media does not have any likes'
+        ]);
+
+        // add a like
+        try{
+            MediaLike::create(['user_id' => self::$user->id, 'media_id' => $media->id]);
+        }catch (\Exception $e){
+            $this->fail('Could not add new like to media');
+            return false;
+        }
+
+        // resend the request
+        $response = $this->call('get', Route('user.media.likes', $parameters));
+        $response->assertStatus(200);
+        $response->assertJsonStructure([
+            [
+                'user_id',
+                'media_id',
+                'user'=> [
+                    'id',
+                    'username',
+                    'first_name',
+                    'last_name',
+                    'profile_status',
+                    'profile' => [
+                        'file_path',
+                        'thumb_path',
+                        'width',
+                        'height',
+                    ]
+                ],
+            ]
+        ]);
     }
 }
