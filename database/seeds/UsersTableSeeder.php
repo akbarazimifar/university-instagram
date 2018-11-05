81e2c84747c4b162ca81e899c8f4ef05b2e637ce
Hooman Hasanzadeh <mr.subscript@gmail.com>
2018-11-05 15:44:13 +0330

    Added user relationship seed

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\UserRelationship;
 
 class UsersTableSeeder extends Seeder
 {
@@ -12,8 +13,11 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         factory(App\User::class, 50)->create()->each(function ($user) {
-            $user->medias()->save(factory(App\Media::class)->make());
+            for ($i = 0; $i < 5; $i++) {
+                $user->medias()->save(factory(App\Media::class)->make());
+            }
             $user->profile()->save(factory(App\UserProfile::class)->make());
         });
+        UserRelationship::create(['follower_id' => 1, 'following_id' => 2, 'is_accepted' => 1]);
     }
 }
