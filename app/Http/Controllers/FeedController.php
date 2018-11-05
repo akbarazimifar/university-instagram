c39e5792f9d480861b4a056dcd6fc273294048bf
Hooman Hasanzadeh <mr.subscript@gmail.com>
2018-11-05 15:44:49 +0330

    Completed homepage feeds method

--- app/Http/Controllers/FeedController.php
+++ app/Http/Controllers/FeedController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Media;
 use App\UserRelationship;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -10,19 +11,30 @@ class FeedController extends Controller
 {
     public function homepage(Request $request)
     {
-        if (!$this->checkUserHasFollowings()) return response([
-            'ok'          => false,
-            'status_code' => 403,
-            'description' => 'You have not followed anyone yet'
-        ], 403);
-
-        // TODO: complete this
+        if (!$this->checkUserHasFollowings()) {
+            return response([
+                'ok'          => false,
+                'status_code' => 403,
+                'description' => 'You have not followed anyone yet'
+            ], 403);
+        }
+        return Media::select('medias.*')
+            ->with([
+                'user',
+                'user.profile'
+            ])
+            ->withCount('likes')
+            ->join('users', 'user_id', '=', 'users.id')
+            ->join('users_relationships', 'users.id', '=', 'users_relationships.following_id')
+            ->where('users_relationships.is_accepted', '=', true)
+            ->orderByDesc('created_at')
+            ->paginate();
     }
 
     protected function checkUserHasFollowings()
     {
-        return (boolean) UserRelationship::whereFollowerId(Auth::user()->id)
-            ->whereIsAccpeted(true)
+        return (bool)UserRelationship::whereFollowerId(Auth::user()->id)
+            ->whereIsAccepted(true)
             ->limit(1)
             ->count();
     }
