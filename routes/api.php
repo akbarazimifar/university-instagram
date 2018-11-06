c12f3ff51078ef3480b1f5e91f3910d613dd5b91
Hooman Hasanzadeh <mr.subscript@gmail.com>
2018-11-06 13:33:54 +0330

    Defined media upload route

--- routes/api.php
+++ routes/api.php
@@ -12,8 +12,11 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::get('feeds', 'FeedController@homepage')->middleware('auth:api')->name('user.feeds');
-Route::get('search', 'UserController@search')->middleware('auth:api')->name('user.search');
+Route::middleware(['auth:api'])->group(function () {
+    Route::get('feeds', 'FeedController@homepage')->name('user.feeds');
+    Route::get('search', 'UserController@search')->name('user.search');
+    Route::post('upload', 'MediaController@upload')->name('user.media.upload');
+});
 Route::middleware(['auth:api', 'username'])->group(function () {
     Route::prefix('{username}')->name('user.')->group(function () {
         Route::get('.', 'UserController@show')->name('profile');
@@ -22,8 +25,8 @@ Route::middleware(['auth:api', 'username'])->group(function () {
 
         Route::get('media/{id}/likes', 'MediaController@getUserMediaLikes')->name('media.likes')->where('id', '[0-9]+');
         Route::patch('media/{id}/like', 'MediaController@likeUserMedia')->name('media.like')->where('id', '[0-9]+');
-        Route::delete('media/{id}/delete','MediaController@delete')->name('media.delete')->where('id', '[0-9]+');
-        Route::patch('media/{id}/edit','MediaController@edit')->name('media.edit')->where('id', '[0-9]+');
+        Route::delete('media/{id}/delete', 'MediaController@delete')->name('media.delete')->where('id', '[0-9]+');
+        Route::patch('media/{id}/edit', 'MediaController@edit')->name('media.edit')->where('id', '[0-9]+');
 
         Route::patch('follow', 'UserController@follow')->name('follow');
         Route::patch('unfollow', 'UserController@unFollow')->name('unfollow');
