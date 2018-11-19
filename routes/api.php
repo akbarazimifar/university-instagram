4b1a50dfaca6d0fb464abab14d72eabbe5cbbde3
Hooman Hasanzadeh <mr.subscript@gmail.com>
2018-11-20 01:25:21 +0330

    Define routes optimized

--- routes/api.php
+++ routes/api.php
@@ -16,27 +16,27 @@ Route::post('register', [\App\Http\Controllers\UserController::class, 'register'
 Route::post('logout', [\App\Http\Controllers\UserController::class, 'logout'])->name('user.logout');
 
 Route::middleware(['auth:api'])->group(function () {
-    Route::get('feeds', 'FeedController@homepage')->name('user.feeds');
-    Route::get('search', 'UserController@search')->name('user.search');
-    Route::post('upload', 'MediaController@upload')->name('user.media.upload');
+    Route::get('feeds', [\App\Http\Controllers\FeedController::class, 'homepage'])->name('user.feeds');
+    Route::get('search', [\App\Http\Controllers\UserController::class, 'search'])->name('user.search');
+    Route::post('upload', [\App\Http\Controllers\MediaController::class, 'upload'])->name('user.media.upload');
 });
 Route::middleware(['auth:api', 'username'])->group(function () {
     Route::prefix('{username}')->name('user.')->group(function () {
-        Route::get('.', 'UserController@show')->name('profile');
-        Route::patch('edit', 'UserController@edit')->name('profile.edit');
+        Route::get('.', [\App\Http\Controllers\UserController::class, 'show'])->name('profile');
+        Route::patch('edit', [\App\Http\Controllers\UserController::class, 'edit'])->name('profile.edit');
 
-        Route::get('medias', 'MediaController@getUserMedias')->name('medias');
-        Route::get('media/{id}', 'MediaController@getUserMedia')->name('media')->where('id', '[0-9]+');
+        Route::get('medias', [\App\Http\Controllers\MediaController::class, 'getUserMedias'])->name('medias');
+        Route::get('media/{id}', [\App\Http\Controllers\MediaController::class, 'getUserMedia'])->name('media')->where('id', '[0-9]+');
 
-        Route::get('media/{id}/likes', 'MediaController@getUserMediaLikes')->name('media.likes')->where('id', '[0-9]+');
-        Route::patch('media/{id}/like', 'MediaController@likeUserMedia')->name('media.like')->where('id', '[0-9]+');
-        Route::delete('media/{id}/delete', 'MediaController@delete')->name('media.delete')->where('id', '[0-9]+');
-        Route::patch('media/{id}/edit', 'MediaController@edit')->name('media.edit')->where('id', '[0-9]+');
+        Route::get('media/{id}/likes', [\App\Http\Controllers\MediaController::class, 'getUserMediaLikes'])->name('media.likes')->where('id', '[0-9]+');
+        Route::patch('media/{id}/like', [\App\Http\Controllers\MediaController::class, 'likeUserMedia'])->name('media.like')->where('id', '[0-9]+');
+        Route::delete('media/{id}/delete', [\App\Http\Controllers\MediaController::class, 'delete'])->name('media.delete')->where('id', '[0-9]+');
+        Route::patch('media/{id}/edit', [\App\Http\Controllers\MediaController::class, 'edit'])->name('media.edit')->where('id', '[0-9]+');
 
-        Route::patch('follow', 'UserController@follow')->name('follow');
-        Route::patch('unfollow', 'UserController@unFollow')->name('unfollow');
+        Route::patch('follow', [\App\Http\Controllers\UserController::class, 'follow'])->name('follow');
+        Route::patch('unfollow', [\App\Http\Controllers\UserController::class, 'unFollow'])->name('unfollow');
 
-        Route::get('followers', 'UserController@followers')->name('followers');
-        Route::get('followings', 'UserController@followings')->name('followings');
+        Route::get('followers', [\App\Http\Controllers\UserController::class, 'followers'])->name('followers');
+        Route::get('followings', [\App\Http\Controllers\UserController::class, 'followings'])->name('followings');
     });
 });
